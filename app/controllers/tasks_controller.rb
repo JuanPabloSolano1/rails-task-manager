8d66fce2f6d683c32d41d110a706c3544cb562ef
juan solano <juanpablosolanosuarez@hotmail.com>
2019-08-13 09:18:33 -0700

    tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,15 @@
 class TasksController < ApplicationController
-   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-   @tasks = Task.all
+    @tasks = Task.all
   end
 
   def show
   end
 
+
   def new
-   @task = Task.new
+    @task = Task.new
   end
 
   def create
@@ -20,17 +21,17 @@ class TasksController < ApplicationController
   end
 
   def update
-   @task.update(task_params)
-   redirect_to task_path(@task)
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
-   @task.destroy
-   redirect_to tasks_path
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def task_params
-   params.require(:task).permit(:title, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed)
   end
 
   def set_task
